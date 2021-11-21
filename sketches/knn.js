0a7bc23990952a268c9558d46af8480f7febde13
Tom Nicholson <tfwnicholson+1@gmail.com>
2021-11-21 18:13:41 +0000

    Make points class aware

--- sketches/knn.js
+++ sketches/knn.js
@@ -8,8 +8,8 @@ const settings = {
 };
 
 const params = {
-  numPoints: 300,
-  k: 21
+  numPoints: 20,
+  k: 7
 }
 
 const dotLT = (A, x) => {
@@ -68,10 +68,17 @@ class Point {
   }
 };
 
+class ClassPoint extends Point {
+  constructor(x, y, cls) {
+    super(x, y);
+    this.cls = cls;
+  }
+};
+
 class Closest {
-  constructor(p, d, c) {
+  constructor(point, d, c) {
     // the point we have
-    this.p = p;
+    this.point = point;
     // the distance it is
     this.d = d;
     // the class it is
@@ -93,24 +100,24 @@ const dist = (p1, p2) => {
   return Math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2);
 }
 
-// cs is a list of Closest, we want to potentially add p to it, so it is less than k
-// and p is closer than the furthest point in cs
-const addKClosest = (cs, p, k) => {
+// cs is a list of Closest, we want to potentially add point to it, so it is less than k
+// and point is closer than the furthest point in cs
+const addKClosest = (cs, point, k) => {
   if(cs.length < k) {
-    cs.push(p)
-  } else if (p.d < cs[cs.length - 1].d) {
-    cs[cs.length - 1] = p;
+    cs.push(point)
+  } else if (point.d < cs[cs.length - 1].d) {
+    cs[cs.length - 1] = point;
     cs.sort((c1, c2) => c1.compare(c2));
   }
 }
 
-const knn = (x, points, k, cls, closest) => {
+const knn = (x, points, k, closest) => {
   if(closest === undefined) {
     closest = [];               //the list of K-closest points
   }
-  points.forEach((p) => {
-    const distance = p.distance(x);
-    const c = new Closest(p, distance, cls);
+  points.forEach((point) => {
+    const distance = point.distance(x);
+    const c = new Closest(point, distance);
     addKClosest(closest, c, k);
   });
 
@@ -126,7 +133,7 @@ const classFromKnnDebug = (cosest) => {
 }
 
 const classFromKnn = (cosest) => {
-  const count = _.countBy(cosest.map((x) => x.c));
+  const count = _.countBy(cosest.map((x) => x.point.cls));
   return _(count)
           .entries()
           .maxBy(_.last)[0];
@@ -135,8 +142,8 @@ const classFromKnn = (cosest) => {
 
 const classToColour = {"1": "red", "2": "green"}
 
-const inBounds = (p, w, h) => {
-  return p[0] >= 0 && p[0] < h && p[1] > 0 && p[1] < w;
+const inBounds = (point, w, h) => {
+  return point[0] >= 0 && point[0] < h && point[1] > 0 && point[1] < w;
 }
 
 const sketch = () => {
@@ -156,22 +163,21 @@ const sketch = () => {
     for(let i = 0; i < params.numPoints; ++i) {
       const point1 = generatePoints2D(width, height, mean1, var1);
       if(point1.containedIn(topLeft, bottomRight)) {
-        points1.push(point1);
+        points1.push(new ClassPoint(point1.x, point1.y, "1"));
       }
       const point2 = generatePoints2D(width, height, mean2, var2);
       if(point2.containedIn(topLeft, bottomRight)) {
-        points2.push(point2);
+        points2.push(new ClassPoint(point2.x, point2.y, "2"));
       }
     }
 
     for(let k = params.k; k >= 1; k -= 2) {
-      console.log(k);
       for(let i = 0; i < height; ++i) {
         for(let j = 0; j < width; ++j) {
           const point = new Point(i, j);
           let closest = [];
-          closest = knn(point, points2, k, 2, closest);
-          closest = knn(point, points1, k, 1, closest);
+          closest = knn(point, points2, k, closest);
+          closest = knn(point, points1, k, closest);
 
           const cls = classFromKnn(closest);
           if(cls === "1") {
@@ -191,10 +197,10 @@ const sketch = () => {
     }
 
     points1.forEach((p) => {
-      // drawPoint(context, p, "black");
+      drawPoint(context, p, "black");
     });
     points2.forEach((p) => {
-      // drawPoint(context, p, "white");
+      drawPoint(context, p, "white");
     });
   };
 };
